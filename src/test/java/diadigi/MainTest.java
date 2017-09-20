69c5a258e381b3fa25236ce4a742e83f4ddb54bd
tkreyling <thomas.kreyling@googlemail.com>
2017-09-21 00:05:54 +0200

    added initial class and test

---
+++ src/test/java/diadigi/MainTest.java
@@ -0,0 +1,13 @@
+package diadigi;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void someTest() {
+        assertTrue(true);
+    }
+}
