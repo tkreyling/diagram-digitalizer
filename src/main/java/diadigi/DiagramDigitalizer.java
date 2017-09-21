4b5fae5e0cfb6f01f8eb0b444da7458c566aa67e
tkreyling <thomas.kreyling@googlemail.com>
2017-09-21 19:21:33 +0200

    added detection of single rectangle

--- src/main/java/diadigi/DiagramDigitalizer.java
+++ src/main/java/diadigi/DiagramDigitalizer.java
@@ -1,10 +1,67 @@
 package diadigi;
 
-import java.util.Collections;
-import java.util.List;
+import org.opencv.core.*;
+import org.opencv.imgcodecs.Imgcodecs;
+
+import java.net.URL;
+import java.util.*;
+
+import static java.util.Collections.emptyList;
+import static java.util.Optional.empty;
+import static java.util.Optional.of;
+import static org.apache.commons.lang3.StringUtils.removeStart;
+import static org.opencv.imgcodecs.Imgcodecs.imread;
+import static org.opencv.imgproc.Imgproc.*;
 
 public class DiagramDigitalizer {
-    public static List<Object> detectContours(String pathToImage) {
-        return Collections.emptyList();
+    static {
+        nu.pattern.OpenCV.loadShared();
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    }
+
+    private static List<Scalar> COLORS = Arrays.asList(
+            new Scalar(255, 0, 0),
+            new Scalar(0, 255, 0),
+            new Scalar(0, 0, 255)
+    );
+
+    private static int INDEX_OF_PARENT = 3;
+
+    public static List<MatOfPoint> detectContours(String pathToImage) {
+        return getPathFor(pathToImage).map(path -> {
+            Mat gray = imread(path, 0);
+
+            Mat threshold = new Mat();
+            threshold(gray, threshold, 180,255, THRESH_BINARY_INV);
+
+            List<MatOfPoint> contours = new ArrayList<>();
+            Mat hierarchy = new Mat();
+            findContours(threshold, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, new Point(0,0));
+
+            for (int i = 0; i < contours.size(); i++) {
+                Mat originalImage = imread(path);
+                drawContours(originalImage, contours, i, COLORS.get(i % COLORS.size()), 2, 8, hierarchy, 0, new Point(0,0));
+                Imgcodecs.imwrite("result-" + i + "-" + removeStart(pathToImage, "/"), originalImage);
+            }
+
+            List<MatOfPoint> result = new ArrayList<>();
+            for (int i = 0; i < contours.size(); i++) {
+                int parentIndex = (int) hierarchy.get(0, i)[INDEX_OF_PARENT];
+                if (parentIndex < 0) {
+                    result.add(contours.get(i));
+                }
+            }
+
+            return result;
+        }).orElse(emptyList());
+    }
+
+    private static Optional<String> getPathFor(String pathOnClasspath) {
+        if (pathOnClasspath == null) return empty();
+
+        URL resource = DiagramDigitalizer.class.getResource(pathOnClasspath);
+        if (resource == null) return empty();
+
+        return of(removeStart(resource.getPath(), "/"));
     }
 }
--- src/test/java/diadigi/DiagramDigitalizerTest.java
+++ src/test/java/diadigi/DiagramDigitalizerTest.java
@@ -2,12 +2,28 @@ package diadigi;
 
 import org.junit.jupiter.api.Test;
 
+import static diadigi.DiagramDigitalizer.detectContours;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DiagramDigitalizerTest {
 
     @Test
     void no_contours_detected_if_path_to_image_is_null() {
-        assertEquals(0, DiagramDigitalizer.detectContours(null).size());
+        assertEquals(0, detectContours(null).size());
+    }
+
+    @Test
+    void no_contours_detected_if_image_does_not_exist() {
+        assertEquals(0, detectContours("/not-existent.jpg").size());
+    }
+
+    @Test
+    void no_contours_detected_if_image_is_empty() {
+        assertEquals(0, detectContours("/empty.jpg").size());
+    }
+
+    @Test
+    void one_rectangle_detected_if_image_contains_one_rectangle() {
+        assertEquals(1, detectContours("/one-rectangle.jpg").size());
     }
 }
